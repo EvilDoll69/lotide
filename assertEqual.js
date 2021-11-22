287bef286680cd80776d2c0d463ae36afb2029a9
EvilDoll69 <tetiana.fediushyn@gmail.com>
2021-11-22 22:56:16 +0000

    assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return (`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return (`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
